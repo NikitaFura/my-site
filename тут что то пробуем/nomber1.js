62a0ed2e3300147ac86e614dabd9013c74e49326
NikitaFura <76919721+NikitaFura@users.noreply.github.com>
2021-01-09 10:50:32 +0100

    Загружаем файлы сайта

---
+++ тут что то пробуем/nomber1.js
@@ -0,0 +1,36 @@
+
+/* let morons = {
+    egor: "жирый",
+    kostya: "тупой",
+    sanya: "программист",
+    barka: "орет"
+};
+
+
+for (let key in morons) {
+    console.log ("Ето мои друганы " + key + " " + morons[key])
+}
+
+
+let arr = [1, 2, 3, 4, 5];
+
+console.log */
+
+/* let ans = prompt ("", ""),
+    arr = [];
+
+arr = ans.split(",");
+console.log(arr); */
+
+/* let shop = ["молоко", "хлеб", "мясо", "пиво", "овощи"] ;
+    
+ shop.pop();
+
+ console.log (shop); */
+
+ let div = document.createElement ("div"),
+    text = document.createTextNode ("Никитос");
+
+    div.classList.add ("black")
+
+    
